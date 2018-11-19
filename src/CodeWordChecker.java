b9bc6fef4500604f190caca3b759fbc06101eafa
BT_1N3_17@BKTECH.LOCAL <BT_1N3_17@BKTECH.LOCAL>
2018-11-19 14:49:57 -0500

    Initial commit

---
+++ src/CodeWordChecker.java
@@ -0,0 +1,26 @@
+public class CodeWordChecker implements StringChecker {
+    private int minLength;
+    private int maxLength;
+    private String badChar;
+
+    public CodeWordChecker(int minLength, int maxLength, String badChar){
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+        this.badChar = badChar;
+    }
+
+    public CodeWordChecker(String badChar){
+        this.badChar = badChar;
+        
+    }
+
+    public boolean isValid(String str){
+        if(str.length() > this.maxLength || str.length() < this.minLength){
+            return false;
+        }
+        if(str.contains(this.badChar)){
+                return false;
+            }
+        return true;
+    }
+}
